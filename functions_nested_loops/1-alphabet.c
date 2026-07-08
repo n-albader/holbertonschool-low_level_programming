cc0441c669be98303f62239aa35bc3621a9f3309
n-albader <noor.abdullah.albader@gmail.com>
2026-07-08 10:40:43 +0300

    Add function to print the alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+
+	_putchar('\n');
+}
