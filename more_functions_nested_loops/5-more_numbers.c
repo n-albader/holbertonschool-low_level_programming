fecaeb2af437c9bc472b7884d6883f598050f069
n-albader <noor.abdullah.albader@gmail.com>
2026-07-10 14:19:34 +0300

    Add function to print more numbers

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 0 to 14 ten times
+ *
+ * Return: Nothing
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j > 9)
+			{
+				_putchar('1');
+			}
+
+			_putchar((j % 10) + '0');
+		}
+
+		_putchar('\n');
+	}
+}
