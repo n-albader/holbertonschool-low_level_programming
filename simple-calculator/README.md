48382b95ff46139853a4be6f4a074e4d9806e3a8
n-albader <noor.abdullah.albader@gmail.com>
2026-07-11 18:01:01 +0300

    Create calculator program skeleton

---
+++ simple-calculator/README.md
@@ -0,0 +1,22 @@
+# Simple Calculator
+
+## Compile Command
+
+gcc -Wall -Werror -Wextra -pedantic -std=gnu89 calculator.c -o calculator
+
+## Run 
+
+./calculator
+
+## Supported Operations
+
+- Display the menu
+- Quit using option 0
+
+## Numeric Behavior
+
+- The program reads integer input using 'scanf'
+
+## Known Limitations
+
+- none 
---
+++ simple-calculator/calculator.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int choice = -1;
+	
+	printf("Simple Calculator\n");
+
+	while (choice !=0)
+	{
+		printf("1) Add\n");
+		printf("2) Subtract\n");
+		printf("3) Multiply\n");
+		printf("4) Divide\n");
+		printf("0) Quit\n");
+		printf("Choice: ");
+
+		scanf("%d", &choice);
+		
+		if (choice ==0)
+			printf("Bye!\n");
+	}
+
+	return (0);
+}
