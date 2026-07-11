67bd4c49dc33e7eaf8969d98da86ff739bd1907d
n-albader <noor.abdullah.albader@gmail.com>
2026-07-11 15:47:47 +0300

    Add program to print first 98 Fibonacci numbers

---
+++ functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 98 Fibonacci numbers
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	unsigned long a1, a2, b1, b2, c1, c2;
+	int i;
+
+	a1 = 0;
+	a2 = 1;
+	b1 = 0;
+	b2 = 2;
+
+	printf("1, 2");
+
+	for (i = 3; i <= 98; i++)
+	{
+		c1 = a1 + b1;
+		c2 = a2 + b2;
+
+		if (c2 >= 1000000000)
+		{
+			c1 += c2 / 1000000000;
+			c2 %= 1000000000;
+		}
+
+		if (c1 > 0)
+			printf(", %lu%09lu", c1, c2);
+		else
+			printf(", %lu", c2);
+
+		a1 = b1;
+		a2 = b2;
+		b1 = c1;
+		b2 = c2;
+	}
+
+	printf("\n");
+
+	return (0);
+}
