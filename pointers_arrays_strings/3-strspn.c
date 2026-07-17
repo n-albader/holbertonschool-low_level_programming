47c8059700b7df12162d3812f0e9c2f8256e61e4
n-albader <noor.abdullah.albader@gmail.com>
2026-07-17 13:53:54 +0300

    Add strspn

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: string to search
+ * @accept: accepted characters
+ *
+ * Return: number of bytes in the initial segment
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				break;
+		}
+
+		if (accept[j] == '\0')
+			break;
+	}
+
+	return (i);
+}
