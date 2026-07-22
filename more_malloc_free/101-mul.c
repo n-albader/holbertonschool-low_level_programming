3c9feb516c248a238d4c00af98b2a0dc9bb32f4a
n-albader <noor.abdullah.albader@gmail.com>
2026-07-22 09:59:02 +0300

    add fixs

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -2,162 +2,136 @@
 #include <stdlib.h>
 
 /**
- * print_error - Prints Error and exits
+ * _errors - handles errors for main
  */
-void print_error(void)
+void _errors(void)
 {
-	char error[] = "Error\n";
-	int i;
+	char *err = "Error\n";
+	int i = 0;
 
-	for (i = 0; error[i] != '\0'; i++)
-		_putchar(error[i]);
-
+	while (err[i])
+	{
+		_putchar(err[i]);
+		i++;
+	}
 	exit(98);
 }
 
 /**
- * is_digit - Checks if a string contains only digits
- * @s: String to check
+ * _is_digit - checks if a string contains only digits
+ * @s: string to be evaluated
  *
- * Return: 1 if true, 0 otherwise
+ * Return: 1 if all digits, 0 otherwise
  */
-int is_digit(char *s)
+int _is_digit(char *s)
 {
-	int i;
+	int i = 0;
 
-	if (*s == '\0')
-		return (0);
-
-	for (i = 0; s[i] != '\0'; i++)
+	while (s[i])
 	{
 		if (s[i] < '0' || s[i] > '9')
 			return (0);
+		i++;
 	}
-
 	return (1);
 }
 
 /**
- * _strlen - Returns length of a string
- * @s: String
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
  *
- * Return: Length
+ * Return: the length of the string
  */
-
 int _strlen(char *s)
 {
-	int len;
+	int i = 0;
 
-	len = 0;
-
-	while (s[len] != '\0')
-		len++;
-
-	return (len);
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
 }
 
 /**
- * init_result - Initializes result array
- * @result: Result array
- * @len: Length
+ * multiply - performs positional multiplication on string digits
+ * @s1: first number string
+ * @s2: second number string
+ * @result: array storing result digits
+ * @len1: length of s1
+ * @len2: length of s2
  */
-void init_result(int *result, int len)
+void multiply(char *s1, char *s2, int *result, int len1, int len2)
 {
-	int i;
-
-	for (i = 0; i < len; i++)
-		result[i] = 0;
-}
-
-/**
- * multiply - Performs multiplication
- * @a: First number
- * @b: Second number
- * @result: Result array
- */
-void multiply(char *a, char *b, int *result)
-{
-
-	int len1, len2, len, i, j;
-  int carry, n1, n2;
-  int sum;
-
-	len1 = _strlen(a);
-	len2 = _strlen(b);
-	len = len1 + len2;
+	int i, j, digit1, digit2, carry;
 
 	for (i = len1 - 1; i >= 0; i--)
 	{
+		digit1 = s1[i] - '0';
 		carry = 0;
-		n1 = a[i] - '0';
-
 		for (j = len2 - 1; j >= 0; j--)
 		{
-			n2 = b[j] - '0';
-			sum = result[i + j + 1] + (n1 * n2) + carry;
-			result[i + j + 1] = sum % 10;
-			carry = sum / 10;
+			digit2 = s2[j] - '0';
+			carry += result[i + j + 1] + (digit1 * digit2);
+			result[i + j + 1] = carry % 10;
+			carry /= 10;
 		}
-
-		result[i] += carry;
+		if (carry > 0)
+			result[i + j + 1] += carry;
 	}
 }
+
 /**
- * print_result - Prints multiplication result
- * @result: Result array
- * @len: Length
+ * print_result - prints the digits of the result array
+ * @result: array containing result digits
+ * @len: total size of the result array
  */
 void print_result(int *result, int len)
 {
-  int i;
-  int started;
-
-  started = 0;
+	int i, printed = 0;
 
 	for (i = 0; i < len; i++)
 	{
-		if (result[i] != 0)
-			started = 1;
-
-		if (started)
+		if (result[i])
+			printed = 1;
+		if (printed)
 			_putchar(result[i] + '0');
 	}
-
-	if (!started)
+	if (!printed)
 		_putchar('0');
-
 	_putchar('\n');
 }
 
 /**
- * main - Multiplies two positive numbers
- * @argc: Argument count
- * @argv: Argument vector
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
- * Return: 0
+ * Return: always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-  int len;
-  int *result;
+	char *s1, *s2;
+	int len1, len2, len, i, *result;
 
-  if (argc != 3)
-    print_error();
+	if (argc != 3 || !_is_digit(argv[1]) || !_is_digit(argv[2]))
+		_errors();
 
-  if (!is_digit(argv[1])|| !is_digit(argv[2]))
-    print_error();
+	s1 = argv[1];
+	s2 = argv[2];
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2;
 
-  len = _strlen(argv[1]) + _strlen(argv[2]);
+	result = malloc(sizeof(int) * len);
+	if (!result)
+		return (1);
 
-  result = malloc(sizeof(int) * len);
-	if (result == NULL)
-		print_error();
+	for (i = 0; i < len; i++)
+		result[i] = 0;
 
-  init_result(result, len);
-  multiply(argv[1], argv[2], result);
-
-  print_result(result, len);
-
+	multiply(s1, s2, result, len1, len2);
+	print_result(result, len);
 	free(result);
 
 	return (0);
