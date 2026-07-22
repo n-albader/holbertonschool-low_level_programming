8748938e57d646d47a043a5c4d4cbac3653d78a5
n-albader <noor.abdullah.albader@gmail.com>
2026-07-22 12:12:13 +0300

    add typedef

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Information about a dog
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
