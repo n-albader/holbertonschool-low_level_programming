d8ad9314cf50a8d191765eac9227fb12a21f90b2
n-albader <noor.abdullah.albader@gmail.com>
2026-07-22 22:36:32 +0300

    add new type

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -14,6 +14,11 @@ struct dog
 	char *owner;
 };
 
+/**
+ * dog_t - New name for struct dog
+ */
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
