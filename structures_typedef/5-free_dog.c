a57cabf003555e67996bf066b00d3e759f1ec1ea
n-albader <noor.abdullah.albader@gmail.com>
2026-07-22 22:44:52 +0300

    add free

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - Frees a dog structure
+ * @d: Pointer to the dog to free
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
