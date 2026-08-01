4a49e1a83aeaf2fa70fe8670732417e560507661
n-albader <noor.abdullah.albader@gmail.com>
2026-08-01 20:18:02 +0300

    fix

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -1,16 +1,17 @@
 section .data
-  hello: db "hello, World", 10
-  len: equ $ - hello
+  hello db "Hello, World", 10
+  len equ $ - hello
 
 section .text
   global main
 
-min:
+main:
   ;start
   mov rax, 1
   mov rdi, 1
   mov rsi, hello
   mov rdx, len
+  syscall
 
   ;end
   mov rax, 60
