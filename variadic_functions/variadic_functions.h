9e83410fcc3427b6fce97c3b95f1585071e3cb96
n-albader <noor.abdullah.albader@gmail.com>
2026-08-01 12:13:11 +0300

    fix variadic_functions.h

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -2,6 +2,16 @@
 #define VARIADIC_FUNCTIONS_H
 
 #include <stdarg.h>
+/**
+ * struct printer - Struct printer
+ * @type: Format specifier
+ * @func: Function pointer
+ */
+typedef struct printer
+{
+	char type;
+	void (*func)(va_list *, char *);
+} printer_t;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
