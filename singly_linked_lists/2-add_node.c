c5acbe845c3ba35cb79a63b9d533cded09b48eb9
n-albader <noor.abdullah.albader@gmail.com>
2026-08-08 11:55:32 +0300

    fix strlen Function

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -12,6 +12,7 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
+	unsigned int len = 0;
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
@@ -24,7 +25,10 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 
-	new_node->len = strlen(str);
+	while (str[len] != '\0')
+		len++;
+
+	new_node->len = len;
 	new_node->next = *head;
 	*head = new_node;
 
