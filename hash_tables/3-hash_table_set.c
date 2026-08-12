b829c449a6a00ce7751c9469b6eddae11c3b6741
n-albader <noor.abdullah.albader@gmail.com>
2026-08-12 13:50:58 +0300

    fix output

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -12,13 +12,33 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+	hash_node_t *current;
 	hash_node_t *new_node;
+	char *new_value;
 	unsigned long int index;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
+	current = ht->array[index];
+
+	while (current != NULL)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			new_value = strdup(value);
+			if (new_value == NULL)
+				return (0);
+
+			free(current->value);
+			current->value = new_value;
+
+			return (1);
+		}
+
+		current = current->next;
+	}
 
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
