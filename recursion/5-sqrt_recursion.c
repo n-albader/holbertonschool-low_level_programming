4a68bd0f591327f6e72f74e550f7395c097dd98c
n-albader <noor.abdullah.albader@gmail.com>
2026-08-16 11:44:57 +0300

    square root of a number

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * sqrt_helper - checks possible square roots
+ * @n: number to find the square root of
+ * @guess: current possible square root
+ *
+ * Return: natural square root, or -1 if none exists
+ */
+int sqrt_helper(int n, int guess)
+{
+	if (guess * guess > n)
+		return (-1);
+
+	if (guess * guess == n)
+		return (guess);
+
+	return (sqrt_helper(n, guess + 1));
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number to find the square root of
+ *
+ * Return: natural square root, or -1 if none exists
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+
+	return (sqrt_helper(n, 1));
+}
