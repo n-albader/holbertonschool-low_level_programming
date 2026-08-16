8a3c19045a2bda2664db8ab516bdd530aa615022
n-albader <noor.abdullah.albader@gmail.com>
2026-08-16 22:07:44 +0300

    appends text at the end of a file

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - appends text to the end of a file
+ * @filename: name of the file
+ * @text_content: text to append
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t written;
+	size_t len;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		len = 0;
+		while (text_content[len] != '\0')
+			len++;
+
+		written = write(fd, text_content, len);
+		if (written != (ssize_t)len)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	if (close(fd) == -1)
+		return (-1);
+
+	return (1);
+}
